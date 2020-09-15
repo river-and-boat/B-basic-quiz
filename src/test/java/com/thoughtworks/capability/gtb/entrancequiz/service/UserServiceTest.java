6c51fea73e8c8c14f1762c13971dcafd3026d0a8
RiverAndBoat <842714673@qq.com>
2020-09-15 12:38:17 +0800

    [Jiang Yuzhou] feat: add unit test for select userInfo and add userInfo

---
+++ src/test/java/com/thoughtworks/capability/gtb/entrancequiz/service/UserServiceTest.java
@@ -0,0 +1,54 @@
+package com.thoughtworks.capability.gtb.entrancequiz.service;
+
+import com.thoughtworks.capability.gtb.entrancequiz.domain.User;
+import com.thoughtworks.capability.gtb.entrancequiz.exception.UserException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+class UserServiceTest {
+
+    @Autowired
+    private UserService userService;
+
+    private User user;
+
+    @BeforeEach
+    void init() {
+        user = User.builder()
+                .name("Jiang Yuzhou").age(25L).avatar("test avatar link")
+                .description("this is a test").build();
+    }
+
+    @Test
+    public void testSaveUser() {
+        User user = userService.saveUser(this.user);
+        assertEquals(1L, user.getId());
+        assertEquals("Jiang Yuzhou", user.getName());
+    }
+
+    @Test
+    public void testGetUserById() {
+        User user = userService.saveUser(this.user);
+        User userResult = userService.getUserById(user.getId());
+        assertEquals(user.getId(), userResult.getId());
+        assertEquals(user.getAge(), userResult.getAge());
+        assertEquals(user.getAvatar(), userResult.getAvatar());
+        assertEquals(user.getName(), userResult.getName());
+        assertEquals(user.getDescription(), userResult.getDescription());
+    }
+
+    @Test
+    public void testGetUserByIdWhenUserNotExist() {
+        UserException userException = assertThrows(UserException.class,
+                () -> userService.getUserById(1L),
+                "Expected doThing() to throw, but it didn't");
+        assertEquals("user is not exist",
+                userException.getExceptionEnum().getErrorMessage());
+    }
+}
